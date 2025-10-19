470dd60098c88e941d83056bc75d6aaa22341419
Michael Williams <hello@mikey.nz>
2025-10-19 22:45:41 +1300

    Sure

--- stack/src/params.rs
+++ stack/src/params.rs
@@ -1,10 +1,22 @@
+use indexmap::IndexMap;
+use rimu::Number;
+
 pub enum ParamType {
     Boolean,
+    String,
+    Number,
+    List { item: Box<ParamType> },
+    Object { value: Box<ParamType> },
 }
 
+#[derive(Clone, PartialEq)]
 pub enum ParamValue {
     Boolean(bool),
+    String(String),
+    Number(Number),
+    List(Vec<ParamValue>),
+    Object(IndexMap<String, ParamValue>),
 }
 
-pub struct ParamTypes(HashMap<String, ParamType>);
-pub struct ParamValues(HashMap<String, ParamValue>);
+pub struct ParamTypes(IndexMap<String, ParamType>);
+pub struct ParamValues(IndexMap<String, ParamValue>);
