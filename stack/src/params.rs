52b302f515e7b8e5aca4bf86843fa87533cc3bf2
Michael Williams <hello@mikey.nz>
2025-10-22 10:54:36 +1300

    More

--- stack/src/params.rs
+++ stack/src/params.rs
@@ -1,5 +1,5 @@
 use indexmap::IndexMap;
-use rimu::{Number, Spanned};
+use rimu::{Number, Spanned, Value};
 
 pub struct ParamName(String);
 
@@ -22,3 +22,44 @@ pub enum ParamValue {
 
 pub struct ParamTypes(IndexMap<Spanned<ParamName>, Spanned<ParamType>>);
 pub struct ParamValues(IndexMap<Spanned<ParamName>, Spanned<ParamValue>>);
+
+pub enum IntoParamTypeError {
+    NotAnObject,
+    HasNoType,
+    ListMissingItem,
+    ObjectMissingValue,
+}
+
+impl TryFrom<Value> for ParamType {
+    type Error = IntoParamTypeError;
+
+    fn try_from(value: Value) -> Result<Self, Self::Error> {
+        let Value::Object(object) = value else {
+            return Err(IntoParamTypeError::NotAnObject);
+        };
+
+        let Some(typ) = object.get("type") else {
+            return Err(IntoParamTypeError::HasNoType);
+        };
+
+        match typ {
+            "boolean" => Ok(ParamType::Boolean),
+            "string" => Ok(ParamType::String),
+            "number" => Ok(ParamType::Number),
+            "list" => {
+                let item = object
+                    .get("item")
+                    .ok_or(IntoParamTypeError::ListMissingItem)?;
+                Ok(ParamType::List { item })
+            }
+            "object" => {
+                let value = object
+                    .get("value")
+                    .ok_or(IntoParamTypeError::ObjectMissingValue)?;
+                Ok(ParamType::Object { value })
+            }
+        }
+    }
+}
+
+pub enum IntoParamValueError {}
