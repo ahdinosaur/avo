e0480abb88372715118bfa7f015641a80e202a28
Michael Williams <hello@mikey.nz>
2025-12-01 15:40:31 +1300

    more

--- apply-stdio/src/lib.rs
+++ apply-stdio/src/lib.rs
@@ -1,7 +1,32 @@
-pub struct ResourceParamsTree;
-pub struct ResourcesTree;
-pub struct ResourceStatesTree;
-pub struct ResourceChangesTree;
-pub struct OperationsTree;
+use lusid_view::Tree;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct ResourceParamsTree(pub Tree);
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct ResourcesTree(pub Tree);
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct ResourceStatesTree(pub Tree);
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct ResourceChangesTree(pub Tree);
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct OperationsTree(pub Tree);
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct OperationsEpochs;
-pub struct OperationApplyProgress;
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum OperationApply {
+    Start,
+    Progress,
+    Complete,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Error {
+    display: String,
+}
